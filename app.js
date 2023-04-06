099427f8bc2e681adc7b2e4cff3b6d3c64e25c71
himalay03 <114144373+himalay03@users.noreply.github.com>
2023-04-06 11:37:42 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+// Require module
+const express = require("express");
+
+// creating express object
+const app = express();
+
+// Handling GET request
+app.get("/", (req, res) => {
+  res.send("Test server is running good !!");
+  res.end();
+});
+
+// Port Number
+const PORT = process.env.PORT || 8080;
+
+// Server setup
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
